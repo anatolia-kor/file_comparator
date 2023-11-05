81c6dcdbdd5487c9195cdfbadcdc5a7194560e4f
anatolia-kor (work station) <anatolia.kor@gmail.com>
2023-11-05 14:37:21 +1200

    midwork code review 2

--- comparator.py
+++ comparator.py
@@ -2,11 +2,19 @@ import os.path as pth
 from os import getcwd, startfile, walk, system
 from easygui import diropenbox
 from sys import stdout
-import os
+
+
+class Exit(Exception):
+    def __init__(self, variable_set_expected=None):
+        if variable_set_expected is not None:
+            self.txt = f'You must set {variable_set_expected}.\n'
+            stdout.write(self.txt)
+        stdout.write('Good bye!')
 
 
 class Comparator(object):
     def __init__(self, old_set_path=None, new_set_path=None, do_compare_size=None):
+        self.global_answer = ''
         self.old_set_path = old_set_path
         self.new_set_path = new_set_path
         self.do_compare_size = do_compare_size
@@ -15,21 +23,34 @@ class Comparator(object):
         self.set_names = {'old_set_path': self.old_set_path,
                           'new_set_path': self.new_set_path}
         self.bs = '\\'
-        self.text_err = 'Cant parse your answer.'
-        self.text_compare = 'Would you like to compare files? ("y" - yes, "n" - no)'
-        self.text_dialog_watch = 'Would you like to watch new files list? ("y" - yes, "n" - no)'
-        self.text_dialog_open = 'Would you like to open some files? ("y" - yes, "n" - no)'
-        self.text_do_compare_size = 'Would you like to compare files by its size? ("y" - yes, "n" - no)'
+        self.text_commands_instruction = ('---------------Command-menu---------------\n'
+                                          'To choose directories to compare enter "choose"\n'
+                                          'To compare directories enter "compare"\n'
+                                          'To watch file list enter "watch"\n'
+                                          'To open files enter "open"\n'
+                                          'To copy files to new directory enter "copy"\n'
+                                          'To exit the program enter "exit!"\n'
+                                          '------------------------------------------\n')
+        self.text_err = 'Cant parse your answer.\n'
+        self.text_dir_err = 'First you must choose directories to compare.\n'
+        self.text_choose_old_dir = 'Choose old directory to compare.\n'
+        self.text_choose_new_dir = 'Choose new directory to compare.\n'
+        self.text_choose_dirs = ('You will be asking to choose directories to compare.\n'
+                                 'Would you like to proceed? ("y" - yes, "n" - no)\n')
+        self.text_compare = 'Would you like to compare files? ("y" - yes, "n" - no)\n'
+        self.text_dialog_watch = 'Would you like to watch new files list? ("y" - yes, "n" - no)\n'
+        self.text_dialog_open = 'Would you like to open some files? ("y" - yes, "n" - no)\n'
+        self.text_do_compare_size = 'Would you like to compare files by its size? ("y" - yes, "n" - no)\n'
         self.text_file_open_instruction = ('To open file enter file number.\n'
                                            'To show file in catalog enter file number and "e".\n'
                                            'To finish opening files print "exit"\n')
         self.text_names = {'text_err': self.text_err,
+                           'text_choose_dirs': self.text_choose_dirs,
                            'text_compare': self.text_compare,
                            'text_dialog_watch': self.text_dialog_watch,
                            'text_dialog_open': self.text_dialog_open,
                            'text_do_compare_size': self.text_do_compare_size,
                            'text_file_open_instruction': self.text_file_open_instruction}
-        self.set_old_new_set_paths()
 
     def __str__(self):
         return f'Comparator {self.old_set_path} with {self.new_set_path}'
@@ -40,25 +61,56 @@ class Comparator(object):
         elif 'text' in item:
             return self.text_names[item]
 
-    def set_old_new_set_paths(self):
-        if self.old_set_path is None:
-            self.old_set_path = diropenbox(title='Choose old directory to compare')
-            self.old_set_path.replace(self.script_dir, '')
-        if self.new_set_path is None:
-            self.new_set_path = diropenbox(title='Choose new directory to compare')
-            self.new_set_path.replace(self.script_dir, '')
+    def work(self):
+        self.global_answer = ''
+        while not self.global_answer == 'exit!':
+            self.global_answer = input(self.text_commands_instruction)
+            match self.global_answer:
+                case 'choose':
+                    self.old_set_path, self.new_set_path, do_compare_size = None, None, None
+                    self.choose()
+                case 'compare':
+                    self.compare()
+                case 'watch':
+                    self.watch()
+                case 'open':
+                    self.open()
+                case 'copy':
+                    self.copy()
+                case 'exit!':
+                    pass
+                case _:
+                    stdout.write(self.text_err)
+        self.exit()
+
+    def exit(self):
+        self.global_answer = 'exit!'
+
+    def choose(self):
+        if self.old_set_path is None or self.new_set_path is None:
+            answer = self.asker('text_choose_dirs')
+            if answer == 'n':
+                return
+        while self.old_set_path is None:
+            stdout.write(self.text_choose_old_dir)
+            self.old_set_path = diropenbox(title=self.text_choose_old_dir)
+        self.old_set_path.replace(self.script_dir, '')
+        while self.new_set_path is None:
+            stdout.write(self.text_choose_new_dir)
+            self.new_set_path = diropenbox(title=self.text_choose_new_dir)
+        self.new_set_path.replace(self.script_dir, '')
         if self.do_compare_size is None:
             answer = self.asker('text_do_compare_size')
-            # answer = input(self.text_do_compare_size)
-            # while not (answer == 'y' or answer == 'n'):
-            #     answer = input(f'{self.text_err}\n{self.text_do_compare_size}')
             self.do_compare_size = answer == 'y'
         print(f'Checking size of files is {"enabled" if self.do_compare_size else "disabled"}')
 
     def asker(self, text_name):
         answer = input(self[text_name])
-        while not (answer == 'y' or answer == 'n'):
-            answer = input(f'{self.text_err}\n{self[text_name]}')
+        while not (answer == 'y' or answer == 'n' or answer == 'exit!'):
+            if answer == 'exit!':
+                self.global_answer = 'exit!'
+                return answer
+            answer = input(f'{self.text_err}{self[text_name]}')
         return answer
 
     def file_namer(self, address, set_name, d_or_f):
@@ -67,8 +119,6 @@ class Comparator(object):
         if self.do_compare_size:
             path += ' {***} ' + str(pth.getsize(self.script_dir + self[set_name] + path))
         return path
-        #
-        # return path + ' {***} ' + (str(pth.getsize(self.script_dir + self[set_name] + path)) if self.do_compare_size else '')
 
     def set_by_path(self, set_name):
         abs_path_os = pth.abspath(self.script_dir + self[set_name])
@@ -85,10 +135,13 @@ class Comparator(object):
         return cur_set
 
     def compare(self):
+        if self.old_set_path is None or self.new_set_path is None or self.do_compare_size is None:
+            stdout.write(self.text_dir_err)
+            return
         answer = self.asker('text_compare')
         if answer != 'y':
-            self.new_files = dict()
             return
+        self.new_files = dict()
         old_set = self.set_by_path('old_set_path')
         print(f'Old set contains {len(old_set)} files and directories')
         new_set = self.set_by_path('new_set_path')
@@ -99,21 +152,15 @@ class Comparator(object):
         sorted_list.sort()
         self.new_files = dict(zip(range(1, len(sorted_list)+1), sorted_list))
 
-    def dialog_watch(self):
+    def watch(self):
         answer = self.asker('text_dialog_watch')
-        # answer = input(self.text_dialog_watch)
-        # while not (answer == 'y' or answer == 'n'):
-        #     answer = input(f'{self.text_err}\n{self.text_dialog_watch}')
         if answer == 'y':
             print('-----------new-files-list-----------')
             [print(i, 'elem: ', elem, end='\n') for i, elem in self.new_files.items()]
             print('------------------------------------')
 
-    def dialog_open(self):
+    def open(self):
         answer = self.asker('text_dialog_open')
-        # answer = input(self.text_dialog_open)
-        # while not (answer == 'y' or answer == 'n'):
-        #     answer = input(f'{self.text_err}\n{self.text_dialog_open}')
         if answer == 'n':
             return
         answer = input(self.text_file_open_instruction)
@@ -129,3 +176,6 @@ class Comparator(object):
             else:
                 print(self.text_err)
             answer = input(self.text_file_open_instruction)
+
+    def copy(self):
+        pass
--- main.py
+++ main.py
@@ -2,33 +2,8 @@ import comparator
 
 
 def test_comparator():
-    # experiment 1
-    # set_a = set([f'abc\\'])
-    # set_b = set([f'abc\\', f'bca\\'])
-    # set_c = set_b - set_a
-    # print('Experiment 1 ', set_c)
-    #
-    # experiment 2
-    # per1 = f'D:\\_python\\_projects\\file_comparator\\test_sets\\test_set_new1\\Новая папка'
-    # per2 = 'bca'
-    # per3 = f'D:\\_python\\_projects\\file_comparator\\test_sets\\test_set_new1'
-    # per4 = f'\\Новая папка'
-    # per5 = per3 + per4
-    # set_a = set()
-    # set_a.add(per1)
-    # set_b = set()
-    # set_b.add(per3)
-    # set_b.add(per5)
-    # set_c = set_b - set_a
-    # print('Experiment 2 ', set_c)
-
-
-    # comp = comparator.Comparator('\\test_sets\\test_set_old', '\\test_sets\\test_set_new')
     comp = comparator.Comparator()
-    # print(comp)
-    comp.compare()
-    comp.dialog_watch()
-    comp.dialog_open()
+    comp.work()
 
 
 if __name__ == '__main__':
